c9d293c963d488e56cc5fd007b66a2b0441274f1
xi <xi@dhcp-215-18.vpn.tu-berlin.de>
2013-07-24 22:37:58 +0800

    error page layout r Please enter the commit message for your changes. Lines starting

--- src/com/example/vinfoexperttest/MainActivity.java
+++ src/com/example/vinfoexperttest/MainActivity.java
@@ -3,20 +3,87 @@ package com.example.vinfoexperttest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.util.Log;
+import android.widget.EditText;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
+	
+	private DefaultHttpClient client=null; 
+	private EditText user=null;
+	private EditText password=null; 
+	private EditText status=null;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.activity_main);
+		user=(EditText)findViewById(R.id.user); 
+		password=(EditText)findViewById(R.id.password); status=(EditText)findViewById(R.id.status);
+		Button send=(Button)findViewById(R.id.send);
+		send.setOnClickListener(onSend);
+		client=new DefaultHttpClient(); 
+	}
+	
+	@Override
+	public void onDestroy() { 
+		super.onDestroy();
+		client.getConnectionManager().shutdown(); 
+	}
+	
+	private String getCredentials() {
+		String u=user.getText().toString(); 
+		String p=password.getText().toString();
+		return "fdsf";
+//		return(Base64.encodeBytes((u+":"+p).getBytes())); 
+	}
+	
+	private void updateStatus() { 
+		try {
+			String s=status.getText().toString();
+			HttpPost post=new HttpPost("https://identi.ca/api/statuses/update.json");
+			post.addHeader("Authorization", "Basic "+getCredentials());
+			List<NameValuePair> form=new ArrayList<NameValuePair>(); 
+			form.add(new BasicNameValuePair("status", s));
+			post.setEntity(new UrlEncodedFormEntity(form, HTTP.UTF_8)); 
+			ResponseHandler<String> responseHandler=new BasicResponseHandler();
+			String responseBody=client.execute(post, responseHandler);
+			JSONObject response=new JSONObject(responseBody); 
+		}
+		catch (Throwable t) {
+			Log.e("Patchy", "Exception in updateStatus()", t);
+			goBlooey(t); 
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	private void goBlooey(Throwable t) {
+		AlertDialog.Builder builder=new AlertDialog.Builder(this);
+		builder.setTitle("Exception!") .setMessage(t.toString()) .setPositiveButton("OK", null) .show();	
 	}
+	
+	private View.OnClickListener onSend=new View.OnClickListener() { 
+		public void onClick(View v) {
+		updateStatus(); 
+		}
+	};
 
 }
